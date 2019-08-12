8567b71a2f271fe921947ca26e087fcc348d953f
Alexis Panyathong <alexispanyathong@gmail.com>
2019-08-12 15:57:28 -0500

    Updates on App.js and ProductContext.js
    I added a few missing things to them. I imported React in ProductContext.js and took out the {} in App.js when trying to import ProductContext.js.

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,3 +1,8 @@
-import { createContext } from 'react';
+import React, { createContext } from 'react';
 
-export const ProductContext = createContext();
+const ProductContext = createContext();
+
+
+export default ProductContext;
+
+
